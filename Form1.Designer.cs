eb602f35711ff34d26e3ebbb0d604b44c4a1fef0
keng <rojwilai01@gmail.com>
2022-10-16 21:24:20 +0700

    design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.lbl_a = new System.Windows.Forms.Label();
+            this.lbl_b = new System.Windows.Forms.Label();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(66, 15);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(125, 27);
+            this.txt_a.TabIndex = 0;
+            // 
+            // lbl_a
+            // 
+            this.lbl_a.AutoSize = true;
+            this.lbl_a.Location = new System.Drawing.Point(10, 18);
+            this.lbl_a.Name = "lbl_a";
+            this.lbl_a.Size = new System.Drawing.Size(17, 20);
+            this.lbl_a.TabIndex = 1;
+            this.lbl_a.Text = "a";
+            // 
+            // lbl_b
+            // 
+            this.lbl_b.AutoSize = true;
+            this.lbl_b.Location = new System.Drawing.Point(10, 55);
+            this.lbl_b.Name = "lbl_b";
+            this.lbl_b.Size = new System.Drawing.Size(18, 20);
+            this.lbl_b.TabIndex = 2;
+            this.lbl_b.Text = "b";
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(66, 52);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(125, 27);
+            this.txt_b.TabIndex = 3;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Location = new System.Drawing.Point(330, 41);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(17, 20);
+            this.lbl_result.TabIndex = 4;
+            this.lbl_result.Text = "0";
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(66, 85);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(125, 29);
+            this.btn_calculate.TabIndex = 5;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.txt_b);
+            this.Controls.Add(this.lbl_b);
+            this.Controls.Add(this.lbl_a);
+            this.Controls.Add(this.txt_a);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txt_a;
+        private Label lbl_a;
+        private Label lbl_b;
+        private TextBox txt_b;
+        private Label lbl_result;
+        private Button btn_calculate;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,14 @@ namespace csharp_tutoruial
         {
             InitializeComponent();
         }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            int a = int.Parse(txt_a.Text);
+            int b = int.Parse(txt_b.Text);
+            result = a + b;
+            lbl_result.Text = result.ToString();
+        }
     }
 }
